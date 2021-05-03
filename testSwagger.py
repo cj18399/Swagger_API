30dd664a2bda4ca01d83334aa359b2cfe46cd33f
cj18399 <36263398+cj18399@users.noreply.github.com>
2021-05-04 00:37:37 +0530

    Update testSwagger.py

--- testSwagger.py
+++ testSwagger.py
@@ -28,14 +28,14 @@ swagger_config = {
     "headers": [],
     "specs": [
         {
-            "endpoint": "https://stagingenvnodesequencer.scenera.live",
+            "endpoint": "apispec_1",
             "route": "/apispec_1.json",
             "rule_filter": lambda rule: True,  # all in
             "model_filter": lambda tag: True,  # all in
         }
     ],
-    #"static_url_path": "/flasgger_static",
-    # "static_folder": "static",  # must be set by user
+    "static_url_path": "/flasgger_static",
+    #"static_folder": "static",  # must be set by user
     "swagger_ui": True,
     "specs_route": "/swagger/",
 }
